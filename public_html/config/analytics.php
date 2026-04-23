b78249a9a7c40598ad1b9e0cac4dff05cbf52828
Eoliz <ludovic@eoliz.fr>
2026-04-23 14:58:11 +0200

    Active GA4 G-YXXJVF3CWN

--- public_html/config/analytics.php
+++ public_html/config/analytics.php
@@ -3,17 +3,16 @@
  * Configuration du tracking et de l'analytique.
  * Desactive par defaut : aucune requete exterieure, aucun cookie.
  */
-
 return [
     // Google Search Console
     'gsc_enabled'           => true,
-    'gsc_verification_code' => 'czchIjp46RR8O0ATPO8hVN0_tQyouwydJ7CvmY_qSg0',  // ex : 'abc123xyz456'
+    'gsc_verification_code' => 'czchIjp46RR8O0ATPO8hVN0_tQyouwydJ7CvmY_qSg0',
 
     // Google Analytics 4
-    'ga4_enabled'           => false,
-    'ga4_measurement_id'    => '',  // ex : 'G-XXXXXXXXXX'
+    'ga4_enabled'           => true,
+    'ga4_measurement_id'    => 'G-YXXJVF3CWN',
 
-    // Google Tag Manager
+    // Google Tag Manager (pas utilise)
     'gtm_enabled'           => false,
     'gtm_container_id'      => '',
 
